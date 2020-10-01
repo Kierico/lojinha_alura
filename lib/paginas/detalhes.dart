0d2296a3aa8f16cb5296ac9f8bb680cc3b254552
Maria Fornagieri <maria.fornagieri@santander.com.br>
2020-10-01 16:36:00 -0300

    aula 5 video 1

--- lib/paginas/detalhes.dart
+++ lib/paginas/detalhes.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:lojinha_alura/modelos/movel.dart';
+import 'package:lojinha_alura/widgets/appbar.dart';
 
 class Detalhes extends StatelessWidget {
+  final Movel movel;
+
+  Detalhes({this.movel});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text('Cheguei na outra página'),
-          FlatButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/carrinho');
-            },
-            child: Text('Agora iremos ao carrinho'),
-          )
-        ],
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('utilidades/assets/imagens/${movel.foto}'),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBarCustomizada(
+          titulo: '',
+          ehPaginaCarrinho: false,
+        ),
+        body: Column(
+          children: [
+            FlatButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/carrinho');
+              },
+              child: Text('Agora iremos ao carrinho'),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/elemento_grid_produtos.dart
+++ lib/widgets/elemento_grid_produtos.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lojinha_alura/modelos/movel.dart';
+import 'package:lojinha_alura/paginas/detalhes.dart';
 import 'package:lojinha_alura/widgets/degrade_elemento_grid_produtos.dart';
 import 'package:lojinha_alura/widgets/imagem_elemento_grid_produtos.dart';
 import 'package:lojinha_alura/widgets/titulo_elemento_grid_produtos.dart';
@@ -11,22 +12,25 @@ class ElementoGridProdutos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(boxShadow: [
-        BoxShadow(spreadRadius: 2, blurRadius: 8, color: Colors.black12)
-      ]),
-      margin: EdgeInsets.all(10),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            ImagemElementoGridProdutos(imagem: movel.foto),
-            DegradeElementoGridProdutos(),
-            TituloElementoGridProdutos(titulo: movel.titulo)
-          ],
-        ),
-      ),
-    );
+    return InkWell(
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => Detalhes(movel: movel)));
+        },
+        child: Container(
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(spreadRadius: 2, blurRadius: 8, color: Colors.black12)
+            ]),
+            margin: EdgeInsets.all(10),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    ImagemElementoGridProdutos(imagem: movel.foto),
+                    DegradeElementoGridProdutos(),
+                    TituloElementoGridProdutos(titulo: movel.titulo)
+                  ],
+                ))));
   }
 }
