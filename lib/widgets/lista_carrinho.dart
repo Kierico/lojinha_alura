eeaec292c60e4d206b4ebe69665b4a47e3539960
Maria Fornagieri <maria.fornagieri@santander.com.br>
2020-10-04 21:45:47 -0300

    aula 8 video 1

--- lib/widgets/lista_carrinho.dart
+++ lib/widgets/lista_carrinho.dart
@@ -3,7 +3,12 @@ import 'package:lojinha_alura/main.dart';
 import 'package:lojinha_alura/modelos/item_carrinho.dart';
 import 'package:lojinha_alura/modelos/movel.dart';
 
-class ListaCarrinho extends StatelessWidget {
+class ListaCarrinho extends StatefulWidget {
+  @override
+  _ListaCarrinhoState createState() => _ListaCarrinhoState();
+}
+
+class _ListaCarrinhoState extends State<ListaCarrinho> {
   final List<ItemCarrinho> carrinho = Inicio.itensCarrinho;
   @override
   Widget build(BuildContext context) {
@@ -22,15 +27,42 @@ class ListaCarrinho extends StatelessWidget {
                   image: AssetImage('utilidades/assets/imagens/${movel.foto}'),
                   height: 92,
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(movel.titulo),
-                      Text('${movel.preco}'),
-                      Text('${item.quantidade}')
-                    ],
+                Expanded(
+                  child: Container(
+                    height: 92,
+                    margin: EdgeInsets.only(left: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(movel.titulo),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('${movel.preco}'),
+                            Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: () => _aumentarQuantidade(item),
+                                  child: Container(
+                                    margin: EdgeInsets.all(8),
+                                    child: Icon(Icons.add, size: 14),
+                                  ),
+                                ),
+                                Text('${item.quantidade}'),
+                                GestureDetector(
+                                  onTap: () => _aumentarQuantidade(item),
+                                  child: Container(
+                                    margin: EdgeInsets.all(8),
+                                    child: Icon(Icons.remove, size: 14),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 )
               ],
@@ -39,5 +71,13 @@ class ListaCarrinho extends StatelessWidget {
         );
       },
     );
+  }
+
+  _aumentarQuantidade(ItemCarrinho item) {
+    item.quantidade++;
+  }
+
+  _diminuirQuantidade(ItemCarrinho item) {
+    item.quantidade--;
   }
 }
