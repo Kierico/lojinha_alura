8d83bd972c7682dd904661a2127d83e23ec2e53e
Maria Fornagieri <maria.fornagieri@santander.com.br>
2020-10-04 22:14:27 -0300

    aula 9 video 1

--- lib/paginas/carrinho.dart
+++ lib/paginas/carrinho.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:lojinha_alura/main.dart';
 import 'package:lojinha_alura/widgets/appbar.dart';
 import 'package:lojinha_alura/widgets/lista_carrinho.dart';
 
@@ -8,18 +10,46 @@ class Carrinho extends StatefulWidget {
 }
 
 class _CarrinhoState extends State<Carrinho> {
+  final formatacaoReais = NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
+
   @override
   Widget build(BuildContext context) {
+    int _valorTotal = _calcularTotal();
+
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBarCustomizada(
         titulo: 'Carrinho',
         ehPaginaCarrinho: true,
       ),
       body: ListaCarrinho(atualiza: atualiza),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        height: 80,
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Total', style: Theme.of(context).textTheme.headline4),
+            Text(formatacaoReais.format(_valorTotal),
+                style: Theme.of(context).textTheme.headline5),
+          ],
+        ),
+      ),
     );
   }
 
   void atualiza() {
     setState(() {});
+  }
+
+  int _calcularTotal() {
+    if (Inicio.itensCarrinho.isNotEmpty) {
+      return Inicio.itensCarrinho
+          .map((item) => item.movel.preco * item.quantidade)
+          .reduce((precoAtual, precoNovo) => precoAtual + precoNovo);
+    }
+
+    return 0;
   }
 }
